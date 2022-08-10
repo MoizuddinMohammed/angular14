06e6235379bf14dba0d1d01b20350ba950dda8c4
MoizuddinMohammed <arbaazvillianz.av@gmail.com>
2022-08-10 21:05:43 +0530

    WIP: registration form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RegistrationsService } from '../registrations.service';
 
+// import custom validator to validate that password and confirm password fields match
+import { ConfirmPasswordValidator } from '../_helpers/must-match.validator';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -21,13 +24,15 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.registerForm = this.formbuilder.group({
       name : ['', Validators.required],
-      username : ['', [Validators.required]],
-      password : ['', [Validators.required]],
+      username : ['', [Validators.required,Validators.email]],
+      password : ['', [Validators.required,Validators.minLength(6)]],
       cpassword : ['', [Validators.required]],
       dob : ['', [Validators.required]],
       mobile : ['', [Validators.required]],
       address : ['', [Validators.required]],
-      pincode : ['', [Validators.required]],
+      pincode : ['', [Validators.required,Validators.minLength(6),Validators.maxLength(6)]],
+    }, {
+        validator: ConfirmPasswordValidator('password', 'cpassword')
     });
   }
   get f(){return this.registerForm.controls}
@@ -54,12 +59,12 @@ export class RegisterComponent implements OnInit {
     name: this.registerForm.value.name,
     pincode: this.registerForm.value.pincode,
     username:this.registerForm.value.username}
-    console.log("Modified DATA:::",regObj)
+    //console.log("Modified DATA:::",regObj)
     // this.registrationsService.listOfEmps=regObj;
     this.registrationsService.listOfEmps.push(regObj);
 
-    console.log("Service DATA:::",this.registrationsService.listOfEmps)
-    // this.router.navigate(['/','login']);
+    //console.log("Service DATA:::",this.registrationsService.listOfEmps)
+    this.router.navigate(['/','employees']);
   }
 
 }
